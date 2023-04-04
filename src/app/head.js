f197e0fb7dd680e31098ea78647397029af56b87
Luigi Lupini <luigi.lupini@gmail.com>
2023-04-04 21:08:19 +0200

    1) Basic `app` setup and structure

---
+++ src/app/head.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Head() {
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      />
+      <meta
+        name="description"
+        content="Google clone application built with Next.js 13 & Tailwind CSS"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <title>Google Clone Next.js 13</title>
+    </>
+  );
+}
